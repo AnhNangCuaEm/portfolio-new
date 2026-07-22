9b071ca80a5f6f1cf29e65ded02195a1f797e130
AnhNangCuaEm <thanhhailth1412@gmail.com>
2026-07-22 20:29:02 +0900

    preparing for renew portfolio

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,12 +6,6 @@ export type { GlassSurfaceProps } from './GlassSurface';
 export { default as GlassHeader } from './GlassHeader';
 export type { GlassHeaderProps } from './GlassHeader';
 
-export { default as GlassButton } from './GlassButton';
-export type { GlassButtonProps } from './GlassButton';
-
-export { default as GlassCard } from './GlassCard';
-export type { GlassCardProps } from './GlassCard';
-
 export { default as SkillProgressBar } from './SkillProgressBar';
 export type { SkillProgressBarProps } from './SkillProgressBar';
 
